44a5a02782a61896b729e0073a39ccdb826177bc
Subhadip Sinha <subhadip.sinha@marlabs.com>
2018-07-18 10:52:28 +0530

    First.java

---
+++ Firstapp.java
@@ -0,0 +1,15 @@
+public class Firstapp
+  {
+     public static void main(String [] args)
+   {
+       int a=10;
+       static int b=90;
+       Firstapp a=new Firstapp();
+       a.m1();
+   }
+  public void m1()
+       {
+         System.out.println(a+" "+b);
+       }
+   }
+   
